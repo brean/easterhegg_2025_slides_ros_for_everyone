19a326a11164a658e47559b7bbd1476c7c307ffb
Andreas Bresser <self@andreasbresser.de>
2025-04-08 17:24:16 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 easter.py && pdflatex main.tex
+python3 easter.py 
+pdflatex main.tex
+# run pdflatex again to use pre-calculated values like page numbers or references
+pdflatex main.tex
